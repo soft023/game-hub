cbf96c7fb38b4ae315cefa0b5b9e5c30790863a4
Sheriffdeen <fasasisheriffdeen@gmail.com>
2023-10-07 22:41:40 +0100

    Responsive Layout Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="green">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem bg="red" area={"nav"}>
+        NAV
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg="green">
+          ASIDE
+        </GridItem>
+      </Show>
+      <GridItem bg="gold" area={"main"}>
+        MAIN
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
