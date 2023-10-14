393171e32012d84e88ee812b1ca8cd30d7481d72
Sheriffdeen <fasasisheriffdeen@gmail.com>
2023-10-14 22:26:53 +0100

    Spinner Initial

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,24 @@
 import useData from "../hooks/useData";
 import GenreItem from "./GenreItem";
 import { Genre } from "../hooks/useGenre";
+import { Spinner } from "@chakra-ui/react";
 
 const GenreList = () => {
   const { isLoading, data, error } = useData<Genre>("/genres");
+
+  if (isLoading)
+    return (
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    );
+
+  if (error) return null;
+
   return data.map((dat, index) => (
     <GenreItem key={index} genres={dat}></GenreItem>
   ));
