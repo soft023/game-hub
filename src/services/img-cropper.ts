9ef2fbf2c8357dd8112b5a71328a8eed5ba06ac3
Sheriffdeen <fasasisheriffdeen@gmail.com>
2023-10-22 02:13:08 +0100

    Handled Games with no Image

--- src/services/img-cropper.ts
+++ src/services/img-cropper.ts
@@ -1,7 +1,7 @@
-import React from 'react'
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
 
 const imgCropper = (imgurl:string) => {
- if (!imgurl) return '';
+ if (!imgurl) return noImage;
 const target = 'media/';
 const startIndex =  imgurl.indexOf(target) + target.length ;
 return imgurl.slice(0,startIndex) + 'crop/600/400/' + imgurl.slice(startIndex);
